0903ccd0b2e094b710a2d950f0d20c28d86c7aef
baragu <baraguantonyy@gmail.com>
2024-07-03 23:10:26 +0300

    update: task 1 creating an interface for student and rendering tables

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -1,14 +1,4 @@
-#!/user/bin/env node
-
-//0. Creating an interface for a student
-
-/**
- * This function creates an interface for student
- * @param {string} firstName
- * @param {number} age
- * @param {string} location
- * @param {string} lastName
- */
+// Define Student interface
 interface Student {
   firstName: string;
   age: number;
@@ -16,8 +6,8 @@ interface Student {
   lastName: string;
 }
 
-// creating students
-const student1: Student= {
+// Creating students
+const student1: Student = {
   firstName: "John",
   age: 20,
   location: "New York",
@@ -31,17 +21,18 @@ const student2: Student = {
   lastName: "Doe"
 };
 
-// array of students
-const tudentsList: student[] = [student1, student2];
+// Array of students
+const studentsList = [student1, student2];
 
 // Rendering a table
-cont table = document.createElement("table");
+const table = document.createElement('table');
 document.body.appendChild(table);
 
 studentsList.forEach((student) => {
   const row = table.insertRow();
   const cell1 = row.insertCell(0);
   const cell2 = row.insertCell(1);
-  cell1.textContent = student.firstName;
-  cell2.textContent = student.location;
+  cell1.innerHTML = student.firstName;
+  cell2.innerHTML = student.location;
 });
+
