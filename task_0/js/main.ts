4bae937a59afd75765f05d6193b432dfa7d3cbb1
baragu <baraguantonyy@gmail.com>
2024-07-03 23:28:19 +0300

    update: task_1 directory

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -1,38 +1,67 @@
-// Define Student interface
 interface Student {
-  firstName: string;
-  age: number;
-  location: string;
-  lastName: string;
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
 }
 
-// Creating students
 const student1: Student = {
-  firstName: "John",
-  age: 20,
-  location: "New York",
-  lastName: "Doe"
-};
+  firstName: 'Antony',
+  lastName: 'Baragu',
+  age: 27,
+  location: 'kENYA'
+}
 
 const student2: Student = {
-  firstName: "Jane",
-  age: 21,
-  location: "Paris",
-  lastName: "Doe"
-};
+  firstName: 'hOTENSIA',
+  lastName: 'Wamathaai',
+  age: 28,
+  location: 'kenya'
+}
 
-// Array of students
-const studentsList = [student1, student2];
+const studentList: Array<Student> = [student1, student2];
 
-// Rendering a table
-const table = document.createElement('table');
-document.body.appendChild(table);
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const th1: HTMLTableCellElement = document.createElement('th');
+const th2: HTMLTableCellElement = document.createElement('th');
 
-studentsList.forEach((student) => {
-  const row = table.insertRow();
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  cell1.innerHTML = student.firstName;
-  cell2.innerHTML = student.location;
+th1.innerText = 'First Name';
+th2.innerText = 'Location';
+th1.style.border = '1px solid gray';
+th2.style.border = '1px solid gray';
+th1.style.padding = '.5rem';
+th2.style.padding = '.5rem';
+table.style.border = '1px solid gray';
+table.style.borderCollapse = 'collapse';
+
+
+
+thead.append(th1);
+thead.append(th2);
+
+table.append(thead);
+
+
+studentList.forEach((student) => {
+  const row: HTMLTableRowElement = document.createElement('tr');
+
+  const column1: HTMLTableCellElement = document.createElement('td');
+  const column2: HTMLTableCellElement = document.createElement('td');
+
+  column1.innerText = student.firstName;
+  column2.innerText = student.lastName;
+
+  column1.style.border = '1px solid gray';
+  column2.style.border = '1px solid gray';
+  column1.style.padding = '.5rem';
+  column2.style.padding = '.5rem';
+
+  row.append(column1);
+  row.append(column2)
+
+  table.append(row);
 });
 
+body.append(table)
---
+++ task_1/README.md
@@ -0,0 +1,8 @@
+# 0x04. Typescript
+__`JavaScript` `TypeScript`__
+
+## RESOURCES
+
+- [TypeScript](https://www.typescriptlang.org/)
+- [TypeScript-in-5-minutes](https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html)
+- [Typescript documentation](https://www.typescriptlang.org/docs/handbook/basic-types.html)
